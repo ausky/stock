73e64289044078abeec96c9893e16984549e33fd
敖海样 <aohaiyang@aohaiyangdeMacBook-Pro.local>
2017-08-14 12:38:05 +0800

    分模块处理，及demo 搭建，完成功能1: 登陆雪球，获取股票 2: 从搜狐获取股票行情数据 3: 简单的 ma及后复权计算

---
+++ stock-market/src/main/java/com/ausky/stock/read/ReadStockInfoMain.java
@@ -0,0 +1,29 @@
+/**
+ * File Name:    ReadStockInfoMain.java
+ * <p>
+ * File Desc:    TODO
+ * <p>
+ * Product AB:   TODO
+ * <p>
+ * Product Name: TODO
+ * <p>
+ * Module Name:  TODO
+ * <p>
+ * Module AB:    TODO
+ * <p>
+ * Author:       敖海样
+ * <p>
+ * History:      2017/8/12 created by hy.ao
+ */
+package com.ausky.stock.read;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hy.ao
+ * Date: 2017/8/12
+ * Time: 11:47
+ * 文件说明：TODO
+ */
+public class ReadStockInfoMain
+{
+}
